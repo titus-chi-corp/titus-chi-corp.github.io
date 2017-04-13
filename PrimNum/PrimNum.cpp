238fa6a4359c45b87015a2fe3ede07cdcc200913
Titus-Chi-Corp <titus.chi.corp@gmail.com>
2017-04-13 16:47:40 +0800

    Update PrimNum.cpp

--- PrimNum/PrimNum.cpp
+++ PrimNum/PrimNum.cpp
@@ -9,18 +9,18 @@ int main()
 	bool arr[num+1]={};
 	int i,j;
 	for(i=2;i<=sqrt(num);i++)
-        if(!arr[i])
-            for(j=i;j<=num;j+=i)
-                if(j%i==0&j!=i)
-                    arr[j]=true;
-    int coun=0;
-    for(i=2;i<=num;i++)
-        if(!arr[i])
+		if(!arr[i])
+			for(j=i;j<=num;j+=i)
+				if(j%i==0&j!=i)
+					arr[j]=true;
+	int coun=0;
+	for(i=2;i<=num;i++)
+		if(!arr[i])
         {
-        	printf("%d ",i);
-        	coun++;
-        	if(coun%7==0)
-        		printf("\n");
+			printf("%d ",i);
+			coun++;
+			if(coun%7==0)
+				printf("\n");
 		}
 	if(coun%7!=0)
 		printf("\n");

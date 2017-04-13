2315c859dab8f0953ec8309a9168bd82a5f067f6
Titus-Chi-Corp <titus.chi.corp@gmail.com>
2017-04-13 16:52:05 +0800

    Update PrimNum.cpp

--- PrimNum/PrimNum.cpp
+++ PrimNum/PrimNum.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 int main()
 {
-	printf("Find prime numbers in range : ");
-	int num;
-	scanf("%d",&num);
-	bool arr[num+1]={};
-	int i,j;
-	for(i=2;i<=sqrt(num);i++)
-		if(!arr[i])
-			for(j=i;j<=num;j+=i)
-				if(j%i==0&j!=i)
-					arr[j]=true;
-	int coun=0;
-	for(i=2;i<=num;i++)
-		if(!arr[i])
+    printf("Find prime numbers in range : ");
+    int num;
+    scanf("%d",&num);
+    bool arr[num+1]={};
+    int i,j;
+    for(i=2;i<=sqrt(num);i++)
+        if(!arr[i])
+            for(j=i;j<=num;j+=i)
+                if(j%i==0&j!=i)
+                    arr[j]=true;
+    int coun=0;
+    for(i=2;i<=num;i++)
+        if(!arr[i])
         {
-			printf("%d ",i);
-			coun++;
-			if(coun%7==0)
-				printf("\n");
-		}
-	if(coun%7!=0)
-		printf("\n");
-	printf("Total : %d prime number(s)\n",coun);
-	system("PAUSE");
+            printf("%d ",i);
+            coun++;
+            if(coun%7==0)
+                printf("\n");
+        }
+    if(coun%7!=0)
+        printf("\n");
+    printf("Total : %d prime number(s)\n",coun);
+    system("PAUSE");
 }
